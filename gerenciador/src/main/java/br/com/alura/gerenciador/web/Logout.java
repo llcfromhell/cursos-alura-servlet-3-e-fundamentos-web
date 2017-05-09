4396f9a5dbf71f030e6f4aa393113af3c37dfd04
Lourenço <llc.from.hell@gmail.com>
2017-05-09 06:49:06 -0300

    Ajustando imports

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -1,18 +1,8 @@
 package br.com.alura.gerenciador.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import br.com.alura.gerenciador.web.utils.Cookies;
 
 //@WebServlet(urlPatterns="/logout")
 //public class Logout extends HttpServlet{
